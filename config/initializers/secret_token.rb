6cc552e2ebc350fc4b7421ff72b7d2b0f43adeaf
hkrdrm <hkr.drm@gmx.com>
2013-06-26 17:18:41 -0500

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arduino::Application.config.secret_key_base = '234ae6ef21fa57a95e9e65ba79d6f2cf56db7980020083045f2adc88177c1c11e03b82c65927108a30bdd00137b8411caa55bf806dd33e8faa1090350b014799'
